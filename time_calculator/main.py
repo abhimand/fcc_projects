943052d6696ec0cac76fb98408372e7580ea6ee7
Abhishek Mandal <abhi.mand@Abbyshacky.local>
2020-11-05 14:52:30 -0600

    fixed readme

--- time_calculator/main.py
+++ time_calculator/main.py
@@ -3,13 +3,11 @@ from time_calculator import add_time
 from unittest import main
 
 
-# print(add_time("8:16 PM", "466:02"))
+print(add_time("8:16 PM", "466:02"))
 # print(add_time("11:59 PM", "24:05"))
 # print(add_time("3:30 PM", "2:12", "Monday"))
 # print(add_time("11:59 PM", "24:05", "Wednesday"))
 # print(add_time("3:30 PM", "2:12", "Monday"))
-
-
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- time_calculator/time_calculator.py
+++ time_calculator/time_calculator.py
@@ -1,30 +1,41 @@
 def add_time(start, duration, day=None):
     ### set up
-    week = {'Sunday': 0, 'Monday': 1, 'Tuesday': 2,
-        'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6} 
-    def findDay(value): 
+    week = {'Sunday': 0, 
+            'Monday': 1, 
+            'Tuesday': 2,
+            'Wednesday': 3, 
+            'Thursday': 4, 
+            'Friday': 5, 
+            'Saturday': 6
+    } 
+    def findDay(value, week): 
         value %= 7
         for day, v in week.items(): 
             if v == value: 
                 return day
+
     ### split strings into readable format
     startDetails= start.split()
     startNums = startDetails[0].split(':')
     durationNums = duration.split(':')
     meridiem = startDetails[1]
+
     ### operate on our numbers
     newHour = int(startNums[0]) + int(durationNums[0])
     newMins = int(startNums[1]) + int(durationNums[1])
     meriCounter = newHour // 12
+
     ### fix hour
     if newHour > 12: 
         newHour %= 12
+
     ### fix minutes
     if newMins > 60: 
         newMins %= 60
         newHour += 1
         if newHour >= 12: 
             meriCounter += 1
+
     ### fix meridiem
     n = meriCounter
     initialMeri = meridiem
@@ -34,12 +45,13 @@ def add_time(start, duration, day=None):
         else: 
             meridiem = 'AM'
         meriCounter -= 1
-    ### fix meridiem
+
+    ### finalize meridiem and/or day
     newDay = ''
     if day is not None:  
         val = week.get(day.title())
         newVal = val + round(n/2)
-        newDay += ', ' + findDay(newVal)
+        newDay += ', ' + findDay(newVal, week)
     
     if initialMeri == 'PM' and meridiem == 'AM' and n == 1:
         meridiem += newDay + ' (next day)' 
@@ -53,7 +65,6 @@ def add_time(start, duration, day=None):
         meridiem += newDay + ' (' + str(round(n/2)) + ' days later)'
     else: 
         meridiem += newDay
-
 
     ### fix strings
     if newMins < 10: 
