e4fd0a2739c5a7ec8685d2e5c3397e215eac6125
Abhishek Mandal <abhi.mand@Abbyshacky.local>
2020-11-13 13:59:19 -0600

    finished, passed all test cases

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -1,16 +1,20 @@
 class Category:
     ledger = []
     name = ''
+    withdrawAmt = 0
 
     def __init__(self, categoryName): 
         # have to initialize to prevent objects sharing same attributes
         self.name = categoryName
         self.ledger = []
+        self.withdrawAmt = 0
 
     def __str__(self):  
-
+        # build first line with star centered name
         string = self.name.center(30, '*') + '\n'
+        # list all transactions in ledger
         for dic in self.ledger:
+            # format amount to two decimal points
             num = "{:.2f}".format(dic['amount'])
             des = dic['description'].ljust(30)
             amt = str(num).rjust(30)
@@ -18,30 +22,25 @@ class Category:
             string += des[0:lenAmt - 1] + ' ' + amt[lenAmt:30] + '\n'
         string += 'Total: ' + str(self.get_balance())
         return string
-        
 
-# "deposit                 900.00\nmilk, cereal, eggs, bac -45.67\nTransfer to Entertainme -20.00\nTotal: 834.33"
-#         ```
-# *************Food*************
-# initial deposit        1000.00
-# groceries               -10.15
-# restaurant and more foo -15.89
-# Transfer to Clothing    -50.00
-# Total: 923.96
-# ```
-
+    # add amount and description to ledger
     def deposit(self, amount, description=''): 
         self.ledger.append({'amount': amount, 'description': description})
 
+    # add withdrawal to ledger, but with negative attached
+    # check if we have enough money to do withdrawl
     def withdraw(self, amount, description=''): 
         if self.check_funds(amount) is True: 
+            self.withdrawAmt += amount
             self.ledger.append({'amount': -1 * amount, 'description': description})
             return True
         return False
 
+    # how much money do we have?
     def get_balance(self) -> int: 
         return sum(transaction.get('amount') for transaction in self.ledger)
     
+    # transfer from one category to another
     def transfer(self, amount, budgetCategory) -> bool: 
         if self.check_funds(amount) is True: 
             self.withdraw(amount, "Transfer to " + budgetCategory.name)
@@ -53,8 +52,58 @@ class Category:
         # if amount is less than balance of budget return 
         return amount <= self.get_balance()
 
-
-
-
 def create_spend_chart(categories):
-    i = 1+1
+    # first line
+    line = 'Percentage spent by category\n'
+    # calculate percentages and create dictionary
+    withdrawTotal = sum(cat.withdrawAmt for cat in categories)
+    percentages = []
+    for cat in categories:
+        percentage = (cat.withdrawAmt/withdrawTotal) * 100
+        percentages.append(percentage)
+    # build graph
+    num = 100
+    while num != -10:
+        # add vertical line
+        line += str(num) + '| '
+        # for each category percentage in the cat dictionary
+        for percent in percentages:
+            if percent > int(num): 
+                line += 'o  '
+            else:
+                line += '   '
+        num -= 10
+        # add space except for 100 which has a extra digit
+        line += '\n '
+        # add a extra space if 0 for margin adjusting
+        if num == 0: 
+            line += ' '
+
+    # might need to adjust
+    # cut off line
+    line += '   -' + '---' * len(percentages)
+
+    # get longest string
+    length = 0
+    for cat in categories: 
+        if len(cat.name) > length: 
+            length = len(cat.name)
+    # add empty space to ends of other names
+    catNames = []
+    for cat in categories: 
+        catNames.append(cat.name.ljust(length, ' '))
+    # keep putting character until end of longest string
+    # put char, if no char, put space
+    for index in range(length):
+        line += '\n     '
+        for name in catNames :
+            line += name[index] + '  '
+    return line
+    
+
+# expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+
+
+
+    
+    
--- budget_app/main.py
+++ budget_app/main.py
@@ -3,10 +3,10 @@ import budget
 from budget import create_spend_chart
 from unittest import main
 
-food = budget.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more foo")
+# food = budget.Category("Food")
+# food.deposit(1000, "initial deposit")
+# food.withdraw(10.15, "groceries")
+# food.withdraw(15.89, "restaurant and more foo")
 # print(food.get_balance())
 # clothing = budget.Category("Clothing")
 # food.transfer(50, clothing)
@@ -17,11 +17,14 @@ food.withdraw(15.89, "restaurant and more foo")
 # auto.withdraw(15)
 # print(auto.check_funds(20))
 
-print(food)
+# print(food.ledger)
 # print(clothing.ledger)
 # print(auto.ledger)
 
-# print(create_spend_chart([food, clothing, auto]))
 
+
+# print(create_spend_chart([food, clothing, auto]))
+# expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+# print(expected)
 # Run unit tests automatically
 main(module='test_module', exit=False)
